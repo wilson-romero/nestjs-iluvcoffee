7a007d40dca2ddae134941386eeb12e593bccf46
wilson-romero <me@wilsonromero.com>
2020-10-17 19:00:52 -0500

    chore: add coffee controller

---
+++ src/coffees/coffees.controller.ts
@@ -0,0 +1,39 @@
+import {
+    Body,
+    Controller,
+    Delete,
+    Get,
+    Param,
+    Patch,
+    Post,
+    Query
+} from '@nestjs/common';
+
+@Controller('coffees')
+export class CoffeesController {
+    @Get()
+    findAll(@Query() paginationQuery) {
+        const { limit, offset } = paginationQuery
+        return `This action returns all coffees. Limit ${limit}, offset: ${offset}`
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return `This action return #${id} coffee`
+    }
+
+    @Post()
+    create(@Body() body) {
+        return body
+    }
+
+    @Patch(':id')
+    update(@Param('id') id: string, @Body() body) {
+        return `This action updates #${id} coffee`
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return `This action removes #${id} coffee`
+    }
+}
